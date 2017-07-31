f8763b65c27295c0fa0944c5d25fa6d14fe901c2
master <Quach.Dai.Tung@usol-v.com.vn>
2017-07-31 15:56:34 +0700

    Add restore with move data to another folder

--- ReleaseToolBox/DatabaseManagement.cs
+++ ReleaseToolBox/DatabaseManagement.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace ReleaseToolBox
 {
@@ -72,10 +67,70 @@ namespace ReleaseToolBox
 
         private void btnRestoreDefault_Click(object sender, EventArgs e)
         {
+            Global global = new Global();
+            string dbinfo = System.IO.File.ReadAllText(@"master.data");
+            if(cbxDatabase.Text == string.Empty)
+            {
+                MessageBox.Show("Please choose or type your database name !!!");
+                return;
+            }
+            Regex regexUserdb = new Regex("..+" + cbxDatabase.Text + "+.*");
+            Match matchUserinfo= regexUserdb.Match(dbinfo);
+            string userInfo = "";
 
+            if (matchUserinfo.Success)
+            {
+                userInfo = matchUserinfo.Value.Replace(Environment.NewLine, "");
+            }
+            else
+            {
+                MessageBox.Show("Can't find your database information. Please check your configuration !!!");
+                return;
+            }
+
+            string userInfoDomainUser = userInfo.Substring(0, userInfo.IndexOf(","));
+
+            if (userInfoDomainUser.Trim() != txtDomainUser.Text.Trim())
+            {
+                DialogResult dialogResult = MessageBox.Show("Warning!!!! It isn't "+ txtDomainUser.Text +" database. You still want to restore! ", "WARNING", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    try
+                    {
+                        string dataPath = userInfo.Substring(userInfo.LastIndexOf(",") + 1, userInfo.Length - userInfo.LastIndexOf(",")-1);
+                        queryExecute(restoreQueryCreate(cbxDatabase.Text,global._dbPath, dataPath.Trim()));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                        return;
+                    }
+                    MessageBox.Show("Restore database sucessfully!!!");
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            else
+            {
+                try
+                {
+                    string dataPath = userInfo.Substring(userInfo.LastIndexOf(",") + 1, userInfo.Length - userInfo.LastIndexOf(",") - 1);
+                    queryExecute(restoreQueryCreate(cbxDatabase.Text, global._dbPath, dataPath.Trim()));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    return;
+                }
+                MessageBox.Show("Restore database sucessfully!!!");
+            }
+            
         }
 
-        private void queryExecute(string datanbaseName, string query)
+        private void queryExecute( string query)
         {
             string connetionString = null;
             Global global = new Global();
@@ -98,6 +153,46 @@ namespace ReleaseToolBox
                 return;
             }
             cnn.Close();
+        }
+
+        private string restoreQueryCreate(string databaseName, string databaseLocation, string desFolder)
+        {
+            string restoreQuery = @"IF  EXISTS (SELECT name FROM sys.databases WHERE name = N'" + cbxDatabase.Text + @"') BEGIN ALTER DATABASE " + cbxDatabase.Text + @" SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+DROP DATABASE " + cbxDatabase.Text + @" END 
+
+DECLARE @Table TABLE (LogicalName varchar(128),[PhysicalName] varchar(128), [Type] varchar, [FileGroupName] varchar(128), [Size] varchar(128), 
+            [MaxSize] varchar(128), [FileId]varchar(128), [CreateLSN]varchar(128), [DropLSN]varchar(128), [UniqueId]varchar(128), [ReadOnlyLSN]varchar(128), [ReadWriteLSN]varchar(128), 
+            [BackupSizeInBytes]varchar(128), [SourceBlockSize]varchar(128), [FileGroupId]varchar(128), [LogGroupGUID]varchar(128), [DifferentialBaseLSN]varchar(128), [DifferentialBaseGUID]varchar(128), [IsReadOnly]varchar(128), [IsPresent]varchar(128), [TDEThumbprint]varchar(128)
+                )
+                DECLARE @Path varchar(1000)='" + databaseLocation + @"'
+                DECLARE @LogicalNameData varchar(128),@LogicalNameLog varchar(128)
+                INSERT INTO @table
+                EXEC('
+                RESTORE FILELISTONLY 
+                   FROM DISK=''' +@Path+ '''
+                   ')
+                
+                   SET @LogicalNameData=(SELECT LogicalName FROM @Table WHERE Type='D')
+                   SET @LogicalNameLog=(SELECT LogicalName FROM @Table WHERE Type='L')
+                
+                RESTORE LABELONLY FROM DISK = '" + databaseLocation + @"';
+                
+                RESTORE FILELISTONLY FROM DISK = '" + databaseLocation + @"' 
+                declare @bak_addr nvarchar(1000);
+                declare @data_addr nvarchar (1000);
+                declare @log_addr nvarchar(1000);
+                set @bak_addr = '" + databaseLocation + @"'
+                set @data_addr = '" + desFolder + @"\'+@LogicalNameData+'.mdf'
+                set @log_addr = '" + desFolder + @"\'+@LogicalNameData+'.ldf'
+                RESTORE DATABASE " + databaseName +  @"
+                FROM DISK = @bak_addr
+                with file =1,
+                MOVE @LogicalNameData to  @data_addr,
+                MOVE @LogicalNameLog TO @log_addr,
+                NOUNLOAD,STATS = 10
+                ";
+
+            return restoreQuery;
         }
     }
 }
